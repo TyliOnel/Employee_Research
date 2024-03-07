30003a65b2db557ce3917f64fbfbb7926f76524a
TyliOnel <tylionel95@gmail.com>
2024-03-07 18:33:18 -0500

    fix

--- EmployeeSQL Table Schemata.sql
+++ EmployeeSQL Table Schemata.sql
@@ -3,19 +3,19 @@
 
 --Create title table
 CREATE TABLE titles (
-    title_id VARCHAR   NOT NULL,
-    title VARCHAR   NOT NULL,
+    title_id VARCHAR(5)   NOT NULL,
+    title VARCHAR(30)   NOT NULL,
     PRIMARY KEY (title_id)
 	);
 
 --Create employees table
 CREATE TABLE employees (
     emp_no INT   NOT NULL,
-    emp_title_id VARCHAR NOT NULL,
+    emp_title_id VARCHAR(5) NOT NULL,
     birth_date DATE   NOT NULL,
-    first_name VARCHAR   NOT NULL,
-    last_name VARCHAR   NOT NULL,
-    sex VARCHAR   NOT NULL,
+    first_name VARCHAR(30)   NOT NULL,
+    last_name VARCHAR(30)   NOT NULL,
+    sex VARCHAR(30)   NOT NULL,
     hire_date DATE   NOT NULL,
     FOREIGN KEY (emp_title_id) REFERENCES titles (title_id),
     PRIMARY KEY (emp_no)
@@ -23,14 +23,14 @@ CREATE TABLE employees (
 
 --Create departments table
 CREATE TABLE departments (
-    dept_no VARCHAR   NOT NULL,
-    dept_name VARCHAR   NOT NULL,
+    dept_no VARCHAR(4)   NOT NULL,
+    dept_name VARCHAR(30)   NOT NULL,
     PRIMARY KEY (dept_no)
 );
 
 --Create dept_manager table
 CREATE TABLE dept_manager (
-    dept_no VARCHAR   NOT NULL,
+    dept_no VARCHAR(4)   NOT NULL,
     emp_no INT   NOT NULL,
     FOREIGN KEY (emp_no) REFERENCES employees (emp_no),
     FOREIGN KEY (dept_no) REFERENCES departments (dept_no),
@@ -40,7 +40,7 @@ CREATE TABLE dept_manager (
 --Create dept_emp table
 CREATE TABLE dept_emp (
     emp_no INT   NOT NULL,
-    dept_no VARCHAR   NOT NULL,
+    dept_no VARCHAR(4)   NOT NULL,
     FOREIGN KEY (emp_no) REFERENCES employees (emp_no),
     FOREIGN KEY (dept_no) REFERENCES departments (dept_no),
     PRIMARY KEY (emp_no, dept_no)
